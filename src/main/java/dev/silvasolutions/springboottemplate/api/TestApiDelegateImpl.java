c7a2f7fb1dab807fde387c90e3be4b7993472699
Trevor Silva <tsilva@techtarget.com>
2023-11-15 13:54:09 -0500

    Add @CrossOrigin to hello-world endpoint as a CORS test

--- src/main/java/dev/silvasolutions/springboottemplate/api/TestApiDelegateImpl.java
+++ src/main/java/dev/silvasolutions/springboottemplate/api/TestApiDelegateImpl.java
@@ -3,6 +3,7 @@ package dev.silvasolutions.springboottemplate.api;
 import dev.silvasolutions.springboottemplate.model.HelloWorld;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * @author tsilva
@@ -11,7 +12,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class TestApiDelegateImpl implements TestApiDelegate {
 
+
   @Override
+  @CrossOrigin(originPatterns = {
+      "https://react-ui-template-target-1-pxfofuxqpq-uk.a.run.app/",
+      "https://xtrevort.com",
+      "http://localhost:3000"
+  })
   public ResponseEntity<HelloWorld> getHelloWorld() {
     var helloWorld = new HelloWorld()
         .title("Hey Jill")
